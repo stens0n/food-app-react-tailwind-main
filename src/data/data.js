357fbbaa83f90e2a6410a297476b077fb4b3b7dc
stens0n <astenson010@gmail.com>
2023-04-26 21:26:03 -0400

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,182 @@
+export const data = [
+    {
+      id: 1,
+      name: 'Chipotle chicken fajita brown rice bowl',
+      category: 'burger',
+      image:
+        'https://www.nutmegnanny.com/wp-content/uploads/2019/04/chipotle-shrimp-fajita-bowl-10.jpg',
+      price: '$$$$',
+    },
+    {
+      id: 2,
+      name: 'Creamy garlic Shrimp with brown rice and fresh zucchini',
+      category: 'burger',
+      image:
+        'https://themovementmenu.com/wp-content/uploads/2019/04/Whole30-Creamy-Garlic-Shrimp-1.jpg',
+      price: '$',
+    },
+    {
+      id: 3,
+      name: 'Lean Ground beef whole wheat penne pasta',
+      category: 'burger',
+      image:
+        'https://embed.widencdn.net/img/beef/razqxi7anw/exact/fresh-tomato-beef-and-penne-pasta-horizontal.tif?keep=c&u=7fueml',
+      price: '$$',
+    },
+    {
+      id: 4,
+      name: 'Keto Buffalo Chicken stuffed peppers',
+      category: 'burger',
+      image:
+        'https://d104wv11b7o3gc.cloudfront.net/wp-content/uploads/2019/08/buffalo-chicken-stuffed-peppers-3.jpg',
+      price: '$$$',
+    },
+    {
+      id: 5,
+      name: '1 lb Roasted Sweet Potatoes',
+      category: 'pizza',
+      image:
+        'https://www.spendwithpennies.com/wp-content/uploads/2022/03/1200-Roasted-Sweet-Potatoes-SpendWithPennies-20.jpg',
+      price: '$$',
+    },
+    {
+      id: 6,
+      name: '1 lb of grilled chicken',
+      category: 'pizza',
+      image:
+        'https://laraclevenger.com/wp-content/uploads/2022/08/Air-Fryer-4-oz-Chicken-Breast-1-683x1024.jpg',
+      price: '$$$',
+    },
+    {
+      id: 7,
+      name: '1 lb of Shrimp',
+      category: 'pizza',
+      image:
+        'https://www.eatwell101.com/wp-content/uploads/2022/03/Sheet-Pan-Shrimp-Recipe-1.jpg',
+      price: '$$$$',
+    },
+    {
+      id: 8,
+      name: '1 lb of Brown Rice',
+      category: 'pizza',
+      image:
+        'https://www.recipetineats.com/wp-content/uploads/2020/09/Perfectly-Cooked-Brown-Rice_7-SQ.jpg',
+      price: '$',
+    },
+    {
+      id: 9,
+      name: 'Kale Salad',
+      category: 'salad',
+      image:
+        'https://images.unsplash.com/photo-1515543237350-b3eea1ec8082?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c2FsYWQlMjBjZWFzYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
+      price: '$$',
+    },
+    {
+      id: 10,
+      name: 'Ceasar Salad',
+      category: 'salad',
+      image:
+        'https://images.unsplash.com/photo-1546793665-c74683f339c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8c2FsYWQlMjBjZWFzYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
+      price: '$$$',
+    },
+    {
+      id: 11,
+      name: 'Loaded Salad',
+      category: 'salad',
+      image:
+        'https://images.unsplash.com/photo-1540420773420-3366772f4999?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2FsYWR8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
+      price: '$$$$',
+    },
+    {
+      id: 12,
+      name: 'Fruit Salad',
+      category: 'salad',
+      image:
+        'https://images.unsplash.com/photo-1564093497595-593b96d80180?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZnJ1aXQlMjBzYWxhZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
+      price: '$',
+    },
+    {
+      id: 13,
+      name: 'Peanut Butter Protein Balls',
+      category: 'chicken',
+      image:
+        'https://www.thespruceeats.com/thmb/2Zt05q7-8QzT2J7hwgKKx6YyfmI=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/protein-balls-5183990-hero-01-280c0b625fbf477e8dec2ca6175b1c23.jpg',
+      price: '$$',
+    },
+    {
+      id: 14,
+      name: 'Keto Protein Strawberry Strudel',
+      category: 'chicken',
+      image:
+        'https://ketopots.com/wp-content/uploads/2021/06/keto-pop-tarts-11.jpg',
+      price: '$$$$',
+    },
+    {
+      id: 15,
+      name: 'Protein Donut',
+      category: 'chicken',
+      image:
+        'https://thebigmansworld.com/wp-content/uploads/2021/11/protein-donuts-recipe.jpeg',
+      price: '$',
+    },
+    {
+      id: 16,
+      name: 'Keto Protein Chips',
+      category: 'chicken',
+      image:
+        'https://res.cloudinary.com/hsxfx8igq/image/upload/t_4x3_recipe_image,f_auto/v1622571101/93115719chips_home_made_1500x1000_6aaa6372-3c2b-40f4-9ba9-2dc14e9d5283_2000x_fw5wkk.jpg',
+      price: '$$$',
+    },
+  ];
+  
+  export const categories = [
+    {
+      id: 1,
+      name: 'Fast Food',
+      image:
+        'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/FastFood_BrowseHome@3x.png',
+    },
+    {
+      id: 2,
+      name: 'Pizza',
+      image:
+        'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Pizza_BrowseHome@3x.png',
+    },
+    {
+      id: 3,
+      name: 'Wings',
+      image:
+        'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Wings_BrowseHome@3x.png',
+    },
+    {
+      id: 4,
+      name: 'Indian',
+      image:
+        'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Indian_BrowseHome@3x.png',
+    },
+    {
+      id: 5,
+      name: 'Latest Deals',
+      image:
+        'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Deals_BrowseHome@3x.png',
+    },
+    {
+      id: 6,
+      name: 'Restaurant Rewards',
+      image:
+        'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/RestoRewards_BrowseHome@3x.png',
+    },
+    {
+      id: 7,
+      name: 'Best Overall',
+      image:
+        'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/TopEats_Browse%20Home@3x.png',
+    },
+    {
+      id: 8,
+      name: 'Shipped Free',
+      image:
+        'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Placeholder_Plates@3x.png',
+    },
+  ];
+  
